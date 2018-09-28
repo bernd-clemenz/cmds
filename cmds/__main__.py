711d1cda3f7da05b7b7a5bd76d11fa3b90ddfe32
bernd-clemenz <bernd.clemenz@virtustream.com>
2018-09-28 15:36:06 +0200

    more dep. improved init.

--- cmds/__main__.py
+++ cmds/__main__.py
@@ -1,5 +1,5 @@
 #
-# simple commands
+# simple remote commands
 # (c) 2018 ISC Clemenz & Weinbrecht GmbH
 #
 
@@ -8,10 +8,14 @@ import cmds
 import sys
 
 if __name__ == '__main__':
+    # 0. command parser.
     arg_parser = argparse.ArgumentParser(description="simple command executor")
     arg_parser.add_argument('--conf', type=str, default='cmds.ini')
     args = arg_parser.parse_args()
 
+    # 1. initialize the module.
     cmds.init(args.conf)
+
+    cmds.LOG.info('done.')
     # a value of zero indicates 'no error' to fauxmo/alexa
     sys.exit(0)
